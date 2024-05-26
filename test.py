64c1c8661593237e2bf6bb6328c1e43aa9a05e45
ykcoda <yawkyereh@outlook.com>
2024-05-26 08:35:54 +0000

    V5: test.py file removed

--- test.py
+++
@@ -1,9 +0,0 @@
-from student import Student
-
-print("Testing the Stiudent class")
-s1 = Student()
-
-s1.FirstName = input("First Name: ")
-s1.LastName = input("Last Name: ")
-
-print(s1.getFullName())
